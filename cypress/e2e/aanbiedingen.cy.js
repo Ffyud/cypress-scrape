b59ad39e29cdcabaae7509a78ce8c201c0e1a8ff
David <5263778+Ffyud@users.noreply.github.com>
2023-09-22 12:25:44 +0200

    add test for creating html file

--- cypress/e2e/aanbiedingen.cy.js
+++ cypress/e2e/aanbiedingen.cy.js
@@ -1,7 +1,16 @@
 describe('Een website openen', () => {
-    it('Doe plus', () => {
+
+    it('Open aanbiedingen pagina', () => {
       cy.visit('https://plus.nl')
 
-      cy.get('[data-title="Aanbiedingen"]').click()
-    })
+      cy.get('[data-title="Aanbiedingen"]').click();
+      cy.get('footer#footer').scrollIntoView({duration: 5000, easing: 'swing'});
+      
+      cy.document().then((doc) => {
+        const fullDOM = doc.documentElement.outerHTML;
+  
+        cy.writeFile('dom-from-cypress.html', fullDOM);
+      });
+    });
+
   });
